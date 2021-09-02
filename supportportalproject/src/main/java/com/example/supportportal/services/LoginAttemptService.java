dfa2540b446125455a06d0a249ad5a6baa5efa30
Rogerr Oliva <Rogerr.oliva@revature.net>
2021-09-02 11:44:55 -0500

    Added Service to lock account if login attempts is exceeds specific amount.

---
+++ supportportalproject/src/main/java/com/example/supportportal/services/LoginAttemptService.java
@@ -0,0 +1,65 @@
+package com.example.supportportal.services;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+/**
+ * This service will cache password attempts in order to lock account after certain attempts
+ **/
+
+@Service
+public class LoginAttemptService {
+
+    private static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
+    private static final int ATTEMPT_INCREMENT = 1;
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    /**
+     * We are initializing the cache with the specific configuration that we need
+     **/
+    public LoginAttemptService() {
+        super();
+        loginAttemptCache = CacheBuilder.newBuilder().expireAfterWrite(15, MINUTES)
+                .maximumSize(100).build(new CacheLoader<String, Integer>() {
+                    @Override
+                    public Integer load(String key) throws Exception {
+                        return 0;
+                    }
+                });
+    }
+
+    /**
+     * Will remove user from the cache
+     **/
+
+    public void evictUserFromLoginAttemptCache(String username) {
+        loginAttemptCache.invalidate(username);
+    }
+
+    /**
+     * Will add user to the LoginAttemptCache. It will add 1 to the current amount cached, and then It will once again add it ot the loginAttemptCache map
+     **/
+    public void addUserToLoginAttemptCache(String username) throws ExecutionException {
+        int attempts = 0;
+        attempts = ATTEMPT_INCREMENT + loginAttemptCache.get(username);
+        loginAttemptCache.put(username, attempts);
+    }
+
+    /**
+     * This will return true if the loginAttempts for that user is greater than the maximum amount allowed.
+     * **/
+    public boolean hasExceededMaxAttempt(String username) throws ExecutionException {
+        return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS;
+    }
+
+
+}
